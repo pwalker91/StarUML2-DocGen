dc338433231aaa4ae36efd099080513e3ff740fc
Peter Walker <pwalker@csumb.edu>
2015-09-18 23:40:01 -0700

    Mirroring how StarUMLJS works.
    This is just a test to see if things work, and what will happen.

--- DocGen/FunctionalSpec.js
+++ DocGen/FunctionalSpec.js
@@ -13,15 +13,46 @@ define(function (require, exports, module) {
     //Importing the StarUML global modules that we want to use
     var Dialogs = app.getModule("dialogs/Dialogs");
 
+    /**
+    * @desc This is the FunctionalSpec object, which we will use to
+    *       catalogue information on the project we've been asked to export
+    * @constructor
+    */
     function FunctionalSpec() {
         this.nothing = null;
     }
 
+    /**
+    * @desc This is a simple function for testing that the object works
+    */
     FunctionalSpec.prototype.test_alert = function test_alert() {
         Dialogs.showAlertDialog("Hello, world!");
     };
 
+
+    /**
+    * @desc This function preforms the high level execution, taking a Project
+    *       element and path, and creating the Functional Specification document
+    * @param    project : type.Project  : __
+    * @param    path    : String?       : __
+    * @return   Boolean : Success(TRUE) or failure(FALSE) of document creation
+    */
+    function execute(project, path) {
+        var result = new $.Deferred();
+
+        console.log(project);
+        console.log(path);
+
+        if ( !(project instanceof type.Project) ) {
+            result.reject();
+        } else {
+            result.resolve()
+        }
+
+        return result.promise();
+    }
+
     //This exports the functions we've defined so that other scripts
     // can use them when executing
-    exports.object = FunctionalSpec;
+    exports.execute = execute;
 });
--- main.js
+++ main.js
@@ -1,57 +1,142 @@
 
-/**
- * Currently a basic Hello World extension for StarUML 2.0
+/*
+ * A Document Generation extension for StarUML 2.0
  *
  * AUTHOR:  Peter Walker
  * DATE:    16 September 2015
  *
  * If you are confused, start here...
  * http://requirejs.org/docs/why.html
+ *
+ * For more information on the API to the StarUML modules, see...
+ * http://starumldocs-7a0.kxcdn.com/2.0.0/api/
+ * https://github.com/staruml/staruml-dev-docs/wiki
+ * http://staruml.io/support
  */
 
 define(function (require, exports, module) {
     "use strict";
 
-    //Importing the StarUML global modules that we want to use,
-    // as well as local scripts that we require for operation
-    var Commands        = app.getModule("command/Commands"),
-        CommandManager  = app.getModule("command/CommandManager"),
-        MenuManager     = app.getModule("menu/MenuManager"),
-        Dialogs         = app.getModule("dialogs/Dialogs"),
-        ExtensionUtils  = app.getModule("utils/ExtensionUtils"),
+    /*
+        Making all of the necessary imports and requires from either local
+        files (we get those with 'require') or from StarUML (app.getModule)
+    */
+
+    //Importing the StarUML global modules that we want to use using the already
+    // globally accessible module 'app', as well as any modules local to this
+    // extension, the Document Generation extension.
+    var Commands            = app.getModule("command/Commands"),
+        CommandManager      = app.getModule("command/CommandManager"),
+        MenuManager         = app.getModule("menu/MenuManager"),
+        Dialogs             = app.getModule("dialogs/Dialogs"),
+        ElementPickerDialog = app.getModule("dialogs/ElementPickerDialog"),
+        FileSystem          = app.getModule("filesystem/FileSystem"),
+        ExtensionUtils      = app.getModule("utils/ExtensionUtils"),
         //Notice that the name of the modules below are the exact same as
         // the name of the files, but DO NOT end with '.js'. This was
         // causing me confusion early on.
-        //What require returns is the 'exports' object that was used (likely)
-        // at the end of the file. In this case, I put the object constructor
-        // for my 'FunctionalSpec' object in the 'exports' object under the
-        // attribute 'object'.
+        //What require returns is the 'exports' object that was modified (likely)
+        // at the end of the file we are requiring. In my case, I put the object
+        // constructor for my 'DesignSpec' object in the 'exports' object under
+        // the attribute 'object'.
         FunctionalSpec  = require("DocGen/FunctionalSpec"),
         DesignSpec      = require("DocGen/DesignSpec");
 
-    //Now I'm actually using the objects I imported using require()
-    // to create an instance, which will be used later on.
+    //Now I'm actually using the objects I have access to after using require.
+    // I will create an instance, which will be used later on.
     //If I had instead put a FUNCTION in an attribute of exports, I could
-    // call it using the appropiate attribute.
-    var FSGen = new FunctionalSpec.object,
-        DSGen = new DesignSpec.object;
+    // call it using the appropiate attribute of the return of require()
+    // e.g. DesignSpec.myfunction()
+    var DSGen = new DesignSpec.object();
 
-    // local handler for Design Spec gen
+
+
+    /*
+        This block creates the functions that we will later associate with commands
+    */
+
+    // local handler for "Not Implemented"
     function localAlert() {
         Dialogs.showAlertDialog("I'm not implemented yet!");
     }
 
-    // Add a Generate Document menu
+    /**
+    * @desc This function creates the dialog box where a user can select
+    *       a project to convert into a document.
+    *       The logic for this function was copied from the StarUMLJS extension
+    */
+    function handleFuncSpecGen(baseElem, path, opts) {
+        //We return a Deferred object at the end, which is a type of AJAX-like
+        // object that other code can attach functions to run when the request
+        // completes/fails/times-out/etc.
+        var result = new $.Deferred();
+
+        //If baseElem is not assigned, popup the ElementPickerDialog dialog box.
+        //Calling showDialog returns a Promise object, so we modify the data
+        // by adding a .done() function.
+        //If the base element is not defined, we ask the user for one
+        if (!baseElem) {
+            ElementPickerDialog.showDialog("Select a base PROJECT to generate from",
+                                           null, type.Project)
+                .done(function (buttonId, selected) {
+                    if (buttonId === Dialogs.DIALOG_BTN_OK && selected) {
+                        baseElem = selected;
+                    }
+                });
+        }
+        //If the user has at least chosen/given an element,
+        if (baseElem && !path) {
+            FileSystem.showOpenDialog(
+                false, true,
+                "Select a folder to place the documents in",
+                null, null,
+                function (err, files) {
+                    if (!err) {
+                        if (files.length > 0) {
+                            path = files[0];
+                        } else {
+                            result.reject(FileSystem.USER_CANCELED);
+                        }
+                    } else {
+                        result.reject(err);
+                    }
+                }
+            );
+        }
+        //And now, if we have both a BASE and a PATH, then we execute.
+        if (baseElem && path) {
+            //What's happening here is that the function 'execute' is executing
+            // when a base element and path have been given.
+            //However, once that is finished, we want to resolve our Deferred object,
+            // which is what has been executing this. Using the .then() function, we
+            // give this code a way to resolve the Deferred object with either a
+            // yay! or nay! at the end (yay=resolve, nay=reject)
+            FunctionalSpec.execute(baseElem, path).then(result.resolve, result.reject);
+        }
+
+        //We finish by returning our Promise object (a Deferred object with
+        // the ability to add functions to only a few listeners)
+        return result.promise();
+    }
+
+
+
+    /*
+        This block add in all of our buttons and commands.
+    */
+
+    // Create the commands that link a user's command (through a button or
+    // shortcut) to a function to execute.
     var CMD_GENERATEDOC    = "tools.generatedoc",
         CMD_GEN_FUNCSPEC   = "tools.generatedoc.funcspec",
         CMD_GEN_DESIGNSPEC = "tools.generatedoc.designspec",
         CMD_GEN_NOTIMP     = "tools.generatedoc.notimp";
     CommandManager.register("Generate Document",        CMD_GENERATEDOC,    CommandManager.doNothing);
-    CommandManager.register("Functional Specification", CMD_GEN_FUNCSPEC,   FSGen.test_alert);
+    CommandManager.register("Functional Specification", CMD_GEN_FUNCSPEC,   handleFuncSpecGen);
     CommandManager.register("Design Specification",     CMD_GEN_DESIGNSPEC, DSGen.test_alert);
     CommandManager.register("Not Implemented!!",        CMD_GEN_NOTIMP,     localAlert);
 
-    // Add Generate Functional Spec menu item (Tools > Generate...)
+    // Add menu items to Tools menu under "Generate Document" (Tools > Generate...)
     var toolsMenu = MenuManager.getMenu(Commands.TOOLS);
     var genMenu = toolsMenu.addMenuItem(CMD_GENERATEDOC, MenuManager.AFTER, toolsMenu.LAST);
     genMenu.addMenuItem(CMD_GEN_FUNCSPEC);
